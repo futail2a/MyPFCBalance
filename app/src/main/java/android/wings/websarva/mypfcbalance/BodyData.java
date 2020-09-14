f92f9a7b07cec6f83f2303d60cc160cd92220c5c
tasato <futail2a@gmail.com>
2020-09-14 23:00:58 +0900

    Create Renfo specific class

--- app/src/main/java/android/wings/websarva/mypfcbalance/BodyData.java
+++ app/src/main/java/android/wings/websarva/mypfcbalance/BodyData.java
@@ -1,9 +1,18 @@
 package android.wings.websarva.mypfcbalance;
 
 public class BodyData {
-    float height;
-    float weight;
-    float bodyFatPercentage;
-    float subcutaneousFatPercentage;
-    float skeletalMusclePercentage;
+    static float m_defaultHeight;
+    float m_height;
+    BodyComposition m_bodyComposition;
+
+    BodyData (float height, BodyComposition bodyComposition){
+        m_defaultHeight= height;
+        m_height = height;
+        m_bodyComposition = bodyComposition;
+    }
+
+    BodyData (BodyComposition bodyComposition){
+        m_height = m_defaultHeight;
+        m_bodyComposition = bodyComposition;
+    }
 }
